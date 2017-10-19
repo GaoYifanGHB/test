fe38562055e06dc6eb7a3fa691c4978984229296
GaoYifanGHB <gao_yifan@163.com>
2017-10-19 22:01:19 +0800

    fg

--- 2dpaint/digitmananger.cpp
+++ 2dpaint/digitmananger.cpp
@@ -3,3 +3,33 @@
 DigitMananger::DigitMananger()
 {
 }
+//初始化，主要功能是
+//1.从所有文件中读出的digit赋值给mould
+//2.清空digitList
+void DigitMananger::initAll()
+{
+    ofstream fs;
+    stringstream ss;
+    string filename;
+    for(int i=0;i<60;i++){
+        ss<<i;
+        ss>>filename;
+        ss.flush();
+        filename="./mat/"+filename+".txt";
+        fs.open(filename.c_str(),ios::in);
+        int l,w;
+        fs>>l>>w;
+        int num=0;
+        bool mat[10][10];
+        for(int i=0;i<l;i++){
+            for(int j=0;j<w;j++){
+                fs>>mat[i][j];
+                if(mat[i][j])num++;
+            }
+        }
+        filename=""
+        fs.close();
+        Digit d(l,w,mat,num);
+        this->mould.push_back(d);
+    }
+}
--- 2dpaint/digitmananger.h
+++ 2dpaint/digitmananger.h
@@ -2,6 +2,10 @@
 #define DIGITMANANGER_H
 #include<vector>
 #include"digit.h"
+#include<fstream>
+#include<sstream>
+#include<cstdlib>
+#include<ctime>
 using namespace std;
 class DigitMananger
 {
@@ -11,15 +15,17 @@ private:
     //digitList用来保存随机选择的digit列表,即要画到屏幕上去的digit
     vector<Digit>digitList;
 
-
+    //在mould中获取一个随机的digit
+    Digit getRandomDigit();
+    //对digitList中的元素进行随机重排
+    void randomSort();
 public:
     DigitMananger();
     //初始化全部
     void initAll();
-    //在mould中获取一个随机的digit
-    Digit getRandomDigit();
     //在(xs-xe)(ys-ye)的矩形中自动添加digit
     void addDigitToRect(int xs,int ys,int xe,int ye);
+
 
 };
 
